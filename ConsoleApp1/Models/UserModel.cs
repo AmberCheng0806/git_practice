ebbce290eb22c33a355e8446bca1a5b14c9c9cd6
周政輝 <leo604leo604@gmail.com>
2025-02-25 15:23:37 +0800

    定義專案架構

---
+++ ConsoleApp1/Models/UserModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1.Models
+{
+    public class UserModel
+    {
+        public String UserID { get; set; }
+        public String Name { get; set; }
+        public String Phone { get; set; }
+        public String Address { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+    }
+}
