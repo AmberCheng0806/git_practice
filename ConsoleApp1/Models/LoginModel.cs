c4d138acb55e18a5b7549baacec40e08563b0037
AmberCheng0806 <qaz29zaq34@gmail.com>
2025-02-25 15:43:32 +0800

    建立登入功能架構

---
+++ ConsoleApp1/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class LoginModel
+    {
+        public string Name { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+
+        private bool CheckPasswordIsValid(string password)
+        {
+            return true;
+        }
+    }
+}
